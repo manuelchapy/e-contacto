e6f2321eb3f15c771c2e2869494a097c8d32d6ff
manuelchapy <manuelchaparro94@gmail.com>
2020-10-31 00:05:01 -0400

    json

--- src/database.js
+++ src/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 //mysql connection
 
 const connection = mysql.createConnection({
-	host: 'localhost',
-	database: 'econtact_db',
-	user: 'root',
-	password: ''
+	host: '74.208.147.248',
+	database: 'db_econtactapp',
+	user: 'dbusr_eca',
+	password: 'Fa5#e6r7'
 })
 
 connection.connect(function(error){
