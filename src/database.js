a053d40a05f230d4d5f86015c70d2a5cab4ddeed
manuelchapy <manuelchaparro94@gmail.com>
2020-12-31 00:53:59 -0400

    DB

--- src/database.js
+++ src/database.js
@@ -2,22 +2,22 @@ const mysql = require('mysql');
 
 //mysql connectionn
 
-/*const connection = mysql.createConnection({
+const connection = mysql.createConnection({
 	host: '74.208.147.248',
 	database: 'db_econtactapp',
 	user: 'dbusr_eca',
 	password: 'Fa5#e6r7'
-})*/
+})
 
 //local host
 
-const connection = mysql.createConnection({
+/*nst connection = mysql.createConnection({
 	host: 'localhost',
 	database: 'econtact_db',
 	user: 'root',
 	password: ''
-})
-//
+})*/
+///////
 connection.connect(function(error){
 	if(error){
 		throw error;
